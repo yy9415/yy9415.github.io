b15a55f372878c787daf7753776402d78ed42c0a
Yu Yang <yangyu9415@gmail.com>
2020-08-12 11:44:35 -0400

    update font

--- assets/sw.js
+++ assets/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202008112240",
+        "/css/pixyll.css?202008121143",
         "/"
       ]);
     })
