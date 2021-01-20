94f85e51e287eda04eb46beaaa069dbc6e2f3f09
Yu Yang <yangyu9415@gmail.com>
2021-01-20 17:09:05 -0500

    update

--- assets/sw.js
+++ assets/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202101021255",
+        "/css/pixyll.css?202101201707",
         "/"
       ]);
     })
